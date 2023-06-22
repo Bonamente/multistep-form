3a4470f98dc08b7558a1dc3b34175292aea6df54
Bonamente <56506035@protonmail.com>
2023-06-22 12:15:01 +0500

    refactor: CustomInput

--- src/components/custom/input/custom-input.tsx
+++ src/components/custom/input/custom-input.tsx
@@ -1,25 +1,30 @@
+import { forwardRef } from 'react';
 import cn from 'classnames';
-
 import style from './custom-input.module.css';
 
 interface CustomInputProps extends React.ComponentProps<'input'> {
   name: string;
-  label?: string;
-  classNames?: string;
+  label: string;
+  classNames: string;
 }
 
-const CustomInput: React.FC<CustomInputProps> = ({
-  name,
-  label,
-  classNames,
-  ...props
-}) => {
-  return (
-    <div className={cn(style.inputContainer, classNames)}>
-      {label ? <label htmlFor={name}>{label}</label> : null}
-      <input className={cn(style.input)} id={name} name={name} {...props} />
-    </div>
-  );
-};
+const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(
+  ({ name, label, classNames = '', ...props }, ref) => {
+    return (
+      <div className={cn(style.inputContainer, classNames)}>
+        <label htmlFor={name}>{label}</label>
+        <input
+          className={cn(style.input)}
+          id={name}
+          name={name}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+CustomInput.displayName = 'CustomInput';
 
 export default CustomInput;
