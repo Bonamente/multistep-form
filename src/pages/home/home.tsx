902455bb83416cc8fc6e91b548a70f575d1f8591
Bonamente <56506035@protonmail.com>
2023-06-22 12:19:06 +0500

    feat: add Home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,19 @@
+import cn from 'classnames';
+import Header from '../../components/header/header';
+import ContactForm from '../../components/contact-form/contact-form';
 import styles from './home.module.css';
 
 const Home = () => {
-  return <div className={styles.title}>Home</div>;
+  return (
+    <div className={cn(styles.pageWrapper, 'container')}>
+      <Header />
+      <main>
+        <div className={cn(styles.pageWrapper)}>
+          <ContactForm />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
